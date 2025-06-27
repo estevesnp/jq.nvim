8ce77893713480a8ff3ebf4224c8a31740943979
estevesnp <nuno.pinto@live.com>
2025-06-27 16:41:24 +0100

    add tab and buffers

--- lua/jq.lua
+++ lua/jq.lua
@@ -1,26 +1,142 @@
 local M = {}
 
-M.setup = function()
-  -- empty
-end
+local state = {
+  input = {
+    buf = nil,
+    expression = ".",
+    filename = nil,
+  },
+  output = {
+    buf = nil,
+  },
+}
+
+local buffers = {
+  input = {
+    kv = {
+      buftype = "acwrite",
+      bufhidden = "wipe",
+      swapfile = false,
+      modifiable = true,
+    },
+    name = "jq-expression",
+  },
+  output = {
+    kv = {
+      buftype = "nofile",
+      bufhidden = "wipe",
+      swapfile = false,
+      modifiable = false,
+      filetype = "json",
+    },
+    name = "jq-result",
+  },
+}
+
+---@class jq.Result
+---@field lines string[]|nil
+---@field error_msg string[]|nil
 
 ---@param filename string
 ---@param expression string
----@return string[]?
-local get_lines = function(filename, expression)
+---@return jq.Result
+local function get_lines(filename, expression)
   local res = vim.system({ "jq", expression, filename }, nil):wait()
 
   if res.stderr and res.stderr ~= "" then
     local stderr_msg = string.match(res.stderr, "^jq: .-error: (.*)$") or res.stderr
-    local msg = string.format("Exit code: %d\nError message: %s", res.code, stderr_msg)
-
-    vim.notify(msg, vim.log.levels.ERROR)
-    return nil
+    return { lines = nil, error_msg = vim.split(stderr_msg, "\n") }
   end
 
-  return vim.split(res.stdout, "\n")
+  return { lines = vim.split(res.stdout, "\n"), error_msg = nil }
 end
 
-vim.print(get_lines(vim.fn.expand("%:p"), "."))
+local function render_output()
+  local res = get_lines(state.input.filename, state.input.expression)
+
+  state.input.error_msg = res.error_msg
+  state.output.lines = res.lines or state.output.lines
+
+  vim.bo[state.output.buf].modifiable = true
+  vim.api.nvim_buf_set_lines(state.output.buf, 0, -1, false, state.output.lines or {})
+  vim.bo[state.output.buf].modifiable = false
+
+  local input_lines
+  if res.error_msg then
+    input_lines = { state.input.expression, "" }
+    vim.list_extend(input_lines, res.error_msg)
+  else
+    input_lines = { state.input.expression }
+  end
+
+  vim.api.nvim_buf_set_lines(state.input.buf, 0, -1, false, input_lines)
+  vim.bo[state.input.buf].modified = false
+end
+
+local function get_input()
+  local lines = vim.api.nvim_buf_get_lines(state.input.buf, 0, 1, false)
+  local joined = table.concat(lines, "\n")
+  state.input.expression = joined
+  vim.bo[state.input.buf].modified = false
+end
+
+local function setup_bufs()
+  for buf_name, buf_config in pairs(buffers) do
+    if state[buf_name].buf == nil or not vim.api.nvim_buf_is_valid(state[buf_name].buf) then
+      local buf = vim.api.nvim_create_buf(false, true)
+
+      for key, value in pairs(buf_config.kv) do
+        vim.bo[buf][key] = value
+      end
+
+      state[buf_name].buf = buf
+
+      vim.api.nvim_buf_set_name(buf, buf_config.name)
+    end
+  end
+
+  vim.cmd("tabnew")
+
+  -- setup output window
+  local output_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(output_win, state.output.buf)
+
+  -- setup input window
+  vim.cmd("split")
+  local input_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(input_win, state.input.buf)
+  vim.api.nvim_win_set_height(input_win, 9)
+
+  vim.api.nvim_create_autocmd("BufWriteCmd", {
+    buffer = state.input.buf,
+    callback = function()
+      get_input()
+      render_output()
+    end,
+  })
+end
+
+---@param filename? string
+function M.open_windows(filename)
+  state.input.filename = filename and vim.fs.abspath(filename) or vim.fn.expand("%:p")
+
+  setup_bufs()
+  render_output()
+end
+
+M.setup = function()
+  if vim.fn.executable("jq") == 0 then
+    vim.notify("'jq' not available, please install it and add it to your PATH", vim.log.levels.ERROR)
+    return
+  end
+
+  vim.api.nvim_create_user_command("JQ", function(opts)
+    local filename = opts.args ~= "" and opts.args or nil
+    M.open_windows(filename)
+  end, {
+    nargs = "?",
+    desc = "Run JQ on file",
+  })
+end
 
 return M
